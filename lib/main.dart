7570b91101e4285ff16b8114c7126f21e4071e20
mai882002 <mai466905@gmail.com>
2024-10-26 23:26:12 +0300

    2 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:mai_store_ecommerce/screen/Homepage.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: 'your_supabase_url',
-    anonKey: 'your_anon_key',
+    url: 'https://vuagprclhrupuinezhva.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1YWdwcmNsaHJ1cHVpbmV6aHZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk1MzE5NDksImV4cCI6MjA0NTEwNzk0OX0.gA-QDURt0VoMpnZWAJcZocX69lcf_VBn1HqDMzmBjKs',
   );
   runApp(MyApp());
 }
